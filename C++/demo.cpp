90d6e6521eec38978f928b3c540eb817450c692d
herveyye <herveyye@gmail.com>
2023-06-21 01:05:02 +0800

    unit4 easy exercise problem6 finished

--- C++/demo.cpp
+++ C++/demo.cpp
@@ -7,10 +7,30 @@ using namespace std;
 inline void keep_window_open() {char ch; cin >> ch;}
 
 int main(){
-    int a {100};
-    char b = {a};
-    char{a};
-    char c = char(128);
-    cout << "hello";
+    double small, large;
+    double num;
+    cin >> num;
+    cout << num << '\n';
+    small = large = num;
+    for(num; cin >> num;){
+        bool judge1 = false, judge2 = false;
+        if(num > large){
+            large = num;
+            judge2 = true;
+        }
+        if(num < small){
+            small = num;
+            judge1 = true;
+        }
+        cout << num << '\n';
+        if(judge1){
+            cout << "the smallest so far\n";
+            judge1 = false;
+        }
+        if(judge2){
+            cout << "the largest so far\n";
+            judge2 = false;
+        }
+    }
     return 0;
 }
