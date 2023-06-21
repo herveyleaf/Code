9c971ca5f9a63dc118c3fa9a5dd0898b0fe1cf9b
herveyye <herveyye@gmail.com>
2023-06-21 14:45:48 +0800

    unit 4 easy exercise finish

--- C++/demo.cpp
+++ C++/demo.cpp
@@ -7,30 +7,73 @@ using namespace std;
 inline void keep_window_open() {char ch; cin >> ch;}
 
 int main(){
+    string kind;
+    int cnt = 0;
+    double sum = 0;
     double small, large;
     double num;
-    cin >> num;
-    cout << num << '\n';
-    small = large = num;
-    for(num; cin >> num;){
+    double mret;
+    bool ctrl = false, yes = false;
+    vector<string> kinds = {"cm", "m", "in", "ft"};
+    vector<double> nums;
+    for(num; cin >> num >> kind;){
+        for(string x : kinds){
+            if(kind == x){
+                yes = true;
+            }
+        }
+        if(yes != true){
+            cout << "wrong value kind";
+            break;
+        }
+        yes = false;
+        char rep = kind[0];
+        switch(rep){
+        case'm':
+            mret = num;
+            break;
+        case'c':
+            mret = num / 100;
+            break;
+        case'i':
+            mret = num * 2.54 / 100;
+            break;
+        case'f':
+            mret = num * 12 * 2.54 / 100;
+            break;
+        }
+        if(ctrl){}
+        else{
+            small = large = mret;
+        }
         bool judge1 = false, judge2 = false;
-        if(num > large){
-            large = num;
+        if(mret > large){
+            large = mret;
             judge2 = true;
         }
-        if(num < small){
-            small = num;
+        if(mret < small){
+            small = mret;
             judge1 = true;
         }
-        cout << num << '\n';
-        if(judge1){
-            cout << "the smallest so far\n";
-            judge1 = false;
+        cout << num << kind << '\n' << "mret = " << mret << "m" << '\n';
+        if(ctrl){
+            if(judge1){
+                cout << "the smallest so far\n";
+                judge1 = false;
+            }
+            if(judge2){
+                cout << "the largest so far\n";
+                judge2 = false;
+            }    
         }
-        if(judge2){
-            cout << "the largest so far\n";
-            judge2 = false;
-        }
+        sum += mret;
+        nums.push_back(mret);
+        cnt++;
+        ctrl = true;
+    }
+    cout << "cnt = " << cnt << '\n' << "sum = " << sum << "m" << '\n';
+    for(double x : nums){
+        cout << x << 'm' << ' ';
     }
     return 0;
 }
