b711e8fae8c4154aa3db2e6120353adade53b11b
herveyye <herveyye@gmail.com>
2023-07-09 10:43:15 +0800

    unit 9

--- C++/demo.cpp
+++ C++/demo.cpp
@@ -1,79 +1,44 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-using namespace std;
-inline void keep_window_open() {char ch; cin >> ch;}
+#include "std_lib_facilities.h"
+
+class Name_pairs{
+public:
+    void read_name(vector<string>);
+    void read_ages();
+    void print();
+    void sort();
+private:
+    string name;
+    double age;
+    vector<string> pair;
+};
+
+void Name_pairs::read_name(vector<string> names){
+    pair = names;
+}
+
+void Name_pairs::read_ages(){
+    string temp;
+    for(int length = pair.size(), cnt = 0; length > 0; length--, cnt++){
+        cin >> temp;
+        pair[cnt] += (' ' + temp);
+    }
+}
+
+void Name_pairs::print(){
+    for(string x : pair){
+        cout << x << '\n';
+    }
+}
 
 int main(){
-    string kind;
-    int cnt = 0;
-    double sum = 0;
-    double small, large;
-    double num;
-    double mret;
-    bool ctrl = false, yes = false;
-    vector<string> kinds = {"cm", "m", "in", "ft"};
-    vector<double> nums;
-    for(num; cin >> num >> kind;){
-        for(string x : kinds){
-            if(kind == x){
-                yes = true;
-            }
-        }
-        if(yes != true){
-            cout << "wrong value kind";
-            break;
-        }
-        yes = false;
-        char rep = kind[0];
-        switch(rep){
-        case'm':
-            mret = num;
-            break;
-        case'c':
-            mret = num / 100;
-            break;
-        case'i':
-            mret = num * 2.54 / 100;
-            break;
-        case'f':
-            mret = num * 12 * 2.54 / 100;
-            break;
-        }
-        if(ctrl){}
-        else{
-            small = large = mret;
-        }
-        bool judge1 = false, judge2 = false;
-        if(mret > large){
-            large = mret;
-            judge2 = true;
-        }
-        if(mret < small){
-            small = mret;
-            judge1 = true;
-        }
-        cout << num << kind << '\n' << "mret = " << mret << "m" << '\n';
-        if(ctrl){
-            if(judge1){
-                cout << "the smallest so far\n";
-                judge1 = false;
-            }
-            if(judge2){
-                cout << "the largest so far\n";
-                judge2 = false;
-            }    
-        }
-        sum += mret;
-        nums.push_back(mret);
-        cnt++;
-        ctrl = true;
+    vector<string> names;
+    string temp;
+    while(cin >> temp && temp != "stop"){
+        names.push_back(temp);
     }
-    cout << "cnt = " << cnt << '\n' << "sum = " << sum << "m" << '\n';
-    for(double x : nums){
-        cout << x << 'm' << ' ';
-    }
+    Name_pairs test;
+    test.read_name(names);
+    test.read_ages();
+    test.print();
     return 0;
 }
